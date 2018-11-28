48d932a1dd690b69c12f6c7c595b08d0b0bf4c1d
Geoff Cheah <geoffcheah@gmail.com>
2018-11-28 21:58:37 +0000

    buttons index

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -1,10 +1,14 @@
 class ButtonsController < ApplicationController
   def index
-    result =  ViewButtons.call
+    # result =  ViewButtons.call
 
-    if result.success?
-      render json: result.buttons, status: :ok
-    else
-      render {}, status: :not_found
+    # if result.success?
+    #   render json: result.buttons, status: :ok
+    # else
+    #   render {}, status: :not_found
+    # end
+    all_buttons = Button.all
+    buttons = all_buttons.map { |button| button.as_json }
+    render json: buttons, status: :ok
   end
 end
