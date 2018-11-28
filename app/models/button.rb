57b470b2be3c42d5212438d39db2215d78076dbe
Geoff Cheah <geoffcheah@gmail.com>
2018-11-28 21:30:13 +0000

    basic model validations and routes set up

--- app/models/button.rb
+++ app/models/button.rb
@@ -1,2 +1,11 @@
 class Button < ApplicationRecord
+
+validates :x, presence: true
+validates :y, presence: true
+validates :height, presence: true
+validates :width, presence: true
+validates :colour, presence: true
+validates :text, presence:true
+validates :click_through_rate, presence:true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope :api, defaults: { format: :json } do
+    scope :v1 do
+      resources :buttons, only: [:create, :index]
+    end
+  end
 end
