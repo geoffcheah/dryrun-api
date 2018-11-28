a100f08dbbf13718597c6e92847768d9bbc99a43
Geoff Cheah <geoffcheah@gmail.com>
2018-11-28 21:45:47 +0000

    index end point and blank interactor

--- app/controllers/buttons_controller.rb
+++ app/controllers/buttons_controller.rb
@@ -1,2 +1,10 @@
 class ButtonsController < ApplicationController
+  def index
+    result =  ViewButtons.call
+
+    if result.success?
+      render json: result.buttons, status: :ok
+    else
+      render {}, status: :not_found
+  end
 end
---
+++ app/interactors/view_buttons.rb
@@ -0,0 +1,11 @@
+class ViewButtons
+  include Interactor
+
+  def call
+    build_button_details
+  end
+
+  def build_button_details
+    # do something
+  end
+end
