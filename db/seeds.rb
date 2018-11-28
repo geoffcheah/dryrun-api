2db81f629dccff86d9b41e65b10a17188d61c665
Geoff Cheah <geoffcheah@gmail.com>
2018-11-28 21:35:54 +0000

    seed file done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Button.create(x: 0, y: 1, height: 100, width: 200, colour: "green", text: "Sign Up", click_through_rate: 20)
+Button.create(x: 0, y: 2, height: 200, width: 300, colour: "red", text: "Sign Up", click_through_rate: 20)
+Button.create(x: 0, y: 3, height: 300, width: 200, colour: "black", text: "Sign Up", click_through_rate: 10)
+Button.create(x: 1, y: 0, height: 100, width: 300, colour: "yellow", text: "Sign Up", click_through_rate: 10)
+Button.create(x: 1, y: 2, height: 140, width: 240, colour: "blue", text: "Sign Up", click_through_rate: 20)
+Button.create(x: 1, y: 3, height: 100, width: 200, colour: "orange", text: "Sign Up", click_through_rate: 40)
+Button.create(x: 2, y: 0, height: 120, width: 200, colour: "teal", text: "Sign Up", click_through_rate: 50)
+Button.create(x: 2, y: 1, height: 100, width: 200, colour: "pink", text: "Sign Up", click_through_rate: 60)
+Button.create(x: 2, y: 2, height: 150, width: 250, colour: "white", text: "Sign Up", click_through_rate: 30)
+Button.create(x: 2, y: 3, height: 100, width: 200, colour: "purple", text: "Sign Up", click_through_rate: 20)
